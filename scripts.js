ad4780154fccd2ac0daa5cae4b902eeeb8543f3f
Rodrigo Barbosa Ponte <rodrigobponte@gmail.com>
2022-05-17 11:43:21 -0300

    Projeto NLW Origin terminado!

--- scripts.js
+++ scripts.js
@@ -5,7 +5,49 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
 }
+
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine =  scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+
+
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // verificar se a base está abaixo do alvo
+  //quais dados vou precisar?
+
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  const sectionBoundaries = sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  } else {
+  }
+
+  // Informações
+
+}
+
+
+
 
 function showNavOnScroll() {
   if (scrollY > 0) {
